42613130914f51970db7f94148aaf6361541c447
komem3 <komata392@gmail.com>
2021-04-11 00:18:21 +0900

    add cmpcheck

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/komem3/gomylint
 go 1.16
 
 require (
+	github.com/komem3/cmpcheck v0.0.0-20210410151635-a640e6c61681
 	github.com/komem3/ctxfirst v0.0.0-20210405150435-78c4fb6f4c34
 	golang.org/x/tools v0.1.0
 )
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/komem3/cmpcheck"
 	"github.com/komem3/ctxfirst"
 	"golang.org/x/tools/go/analysis/multichecker"
 )
 
 func main() {
-	multichecker.Main(ctxfirst.Analyzer)
+	multichecker.Main(
+		ctxfirst.Analyzer,
+		cmpcheck.Analyzer,
+	)
 }
